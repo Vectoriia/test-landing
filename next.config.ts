da36116536b554175b98726a32e07ff3374dc79b
Vectoriia <viktoriamonastyrska@gmail.com>
2025-03-03 22:11:20 +0200

    fix: test route

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from 'next';
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  basePath: isProd ? '/test-landing' : '',
-  assetPrefix: isProd ? '/test-landing' : '',
+  basePath: '/test-landing',
+  assetPrefix: isProd ? '/test-landing/' : '',
   output: 'export',
   distDir: 'dist',
   images: {
